920084cd009461bb1388a68c45bfd534a5a8d3d1
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2022-09-21 20:55:35 +0300

    add bem metod

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalmodal__btn: document.querySelector('[data-modal-open]'),
+    closeModalmodal__btn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalmodal__btn.addEventListener('click', toggleModal);
+  refs.closeModalmodal__btn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
